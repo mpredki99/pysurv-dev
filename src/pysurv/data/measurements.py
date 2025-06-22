d38fd60dafa70dc1cc86986abc98381bc8ea68a7
mpredki99 <mpredki99@gmail.com>
2025-06-22 18:33:26 +0200

    Remove to_float method

--- src/pysurv/data/measurements.py
+++ src/pysurv/data/measurements.py
@@ -12,16 +12,34 @@ class Measurements(pd.DataFrame):
 
         if {"stn_pk", "trg_id"}.issubset(self.columns):
             self.set_index(["stn_pk", "trg_id"], inplace=True)
-            self._to_float()
             self.angles_to_rad()
 
     def _validate_angle_unit(self, angle_unit):
         if angle_unit not in ["rad", "grad", "gon", "deg"]:
             raise ValueError("Angle unit must be either 'rad', 'grad', 'gon', 'deg'.")
 
-    def _to_float(self):
-        for col in self.columns:
-            self[col] = self[col].astype(float)
+    @property
+    def _constructor(self):
+        return Measurements
+
+    # Public interface
+    @property
+    def linear_measurements_columns(self):
+        return self.columns[
+            self.columns.isin(MeasurementModel.COLUMN_LABELS["linear_measurements"])
+        ]
+
+    @property
+    def linear_sigma_columns(self):
+        return self.columns[
+            self.columns.isin(
+                MeasurementModel.COLUMN_LABELS["linear_measurements_sigma"]
+            )
+        ]
+
+    @property
+    def linear_columns(self):
+        return self.linear_measurements_columns.append(self.linear_sigma_columns)
 
     @property
     def angular_measurements_columns(self):
@@ -42,30 +60,8 @@ class Measurements(pd.DataFrame):
         return self.angular_measurements_columns.append(self.angular_sigma_columns)
 
     @property
-    def linear_measurements_columns(self):
-        return self.columns[
-            self.columns.isin(MeasurementModel.COLUMN_LABELS["linear_measurements"])
-        ]
-
-    @property
-    def linear_sigma_columns(self):
-        return self.columns[
-            self.columns.isin(
-                MeasurementModel.COLUMN_LABELS["linear_measurements_sigma"]
-            )
-        ]
-
-    @property
-    def linear_columns(self):
-        return self.linear_measurements_columns.append(self.linear_sigma_columns)
-
-    @property
     def angle_unit(self):
         return self._angle_unit
-
-    @property
-    def _constructor(self):
-        return Measurements
 
     @angle_unit.setter
     def angle_unit(self, new_angle_unit):
