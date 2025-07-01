00bd1f5703a2a784616ce086c9874f404bf05fe0
mpredki99 <mpredki99@gmail.com>
2025-07-01 06:27:41 +0200

    Set default angle_unit from package config.

--- src/pysurv/basic/basic.py
+++ src/pysurv/basic/basic.py
@@ -1,19 +1,26 @@
 import numpy as np
 
+from pysurv import config
+from pysurv.models import validate_angle_unit
 
-def to_rad(angle: float, unit: str = "grad") -> float:
+
+def to_rad(angle: float, unit: str | None = None) -> float:
+    unit = config.angle_unit if unit is None else validate_angle_unit(unit)
+
     if unit in ["grad", "gon"]:
         return angle * np.pi / 200
     elif unit == "deg":
         return angle * np.pi / 180
     else:
-        raise ValueError('Invalid unit. Use "grad", "gon", or "deg".')
+        return angle
 
 
-def from_rad(angle: float, unit: str = "grad") -> float:
+def from_rad(angle: float, unit: str | None = None) -> float:
+    unit = config.angle_unit if unit is None else validate_angle_unit(unit)
+
     if unit in ["grad", "gon"]:
         return angle * 200 / np.pi
     elif unit == "deg":
         return angle * 180 / np.pi
     else:
-        raise ValueError('Invalid unit. Use "grad", "gon", or "deg".')
+        return angle
