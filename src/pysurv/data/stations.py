1c3daa80d766f4e335808f19d90f73d8129eeef9
mpredki99 <mpredki99@gmail.com>
2025-07-03 20:01:18 +0200

    Add approximate orientation calculation

--- src/pysurv/data/stations.py
+++ src/pysurv/data/stations.py
@@ -1,4 +1,9 @@
+import numpy as np
 import pandas as pd
+
+from pysurv.basic import azimuth, from_rad
+from pysurv.config import config
+from pysurv.models import validate_angle_unit
 
 
 class Stations(pd.DataFrame):
@@ -11,3 +16,32 @@ class Stations(pd.DataFrame):
     @property
     def _constructor(self):
         return Stations
+
+    def approx_oreintation_constant(self, hz, controls):
+        stn_pk_loc = hz.index.names.index("stn_pk")
+        trg_id_loc = hz.index.names.index("trg_id")
+
+        for idx, hz_value in hz.items():
+            stn_pk = idx[stn_pk_loc]
+            stn_id = self.at[stn_pk, "stn_id"]
+            trg_id = idx[trg_id_loc]
+
+            x_first, y_first = controls.at[stn_id, "x"], controls.at[stn_id, "y"]
+            x_second, y_second = controls.at[trg_id, "x"], controls.at[trg_id, "y"]
+            self.at[stn_pk, "orientation"] = (
+                azimuth(x_first, y_first, x_second, y_second) - hz_value
+            )
+
+    def display(self, angle_unit=None):
+        col = "orientation"
+        data = self.copy()
+
+        if col not in data.columns:
+            return data
+
+        angle_unit = (
+            config.angle_unit if angle_unit is None else validate_angle_unit(angle_unit)
+        )
+        data[col] = from_rad(np.mod(data[col], 2 * np.pi), unit=angle_unit)
+
+        return data
