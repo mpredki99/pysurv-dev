108b8d6a2cb4f2bd3e69b6aeb30beecd567a96b3
mpredki99 <mpredki99@gmail.com>
2025-07-12 17:12:24 +0200

    Reformat the file. Add to dataframe method.

--- src/pysurv/data/stations.py
+++ src/pysurv/data/stations.py
@@ -18,16 +18,24 @@ class Stations(pd.DataFrame):
         return Stations
 
     def append_oreintation_constant(self, data, controls):
-        df1 = data.copy()
         data = data.reset_index()
         data.insert(1, "stn_id", data.stn_pk.map(self.stn_id))
-        
-        cols = ["stn_id", "trg_id"]
+
+        cols = ["stn", "trg"]
         for point in cols:
-            data = data.merge(controls[["x", "y"]], how='left', left_on=point, right_on="id", suffixes=[f"_{col}" for col in cols])
+            data = data.merge(
+                controls[["x", "y"]],
+                how="left",
+                left_on=f"{point}_id",
+                right_on="id",
+                suffixes=[f"_{col}" for col in cols],
+            )
         data = data.set_index("stn_pk")
-        
-        self["orientation"] = azimuth(data["x_stn_id"], data["y_stn_id"], data["x_trg_id"], data["y_trg_id"]) - data["hz"]
+
+        self["orientation"] = (
+            azimuth(data["x_stn"], data["y_stn"], data["x_trg"], data["y_trg"])
+            - data["hz"]
+        )
 
     def display(self, angle_unit=None):
         col = "orientation"
@@ -42,3 +50,6 @@ class Stations(pd.DataFrame):
         data[col] = from_rad(np.mod(data[col], 2 * np.pi), unit=angle_unit)
 
         return data
+
+    def to_dataframe(self):
+        return pd.DataFrame(self)
