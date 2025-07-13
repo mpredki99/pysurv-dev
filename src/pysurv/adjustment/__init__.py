7b88990184f79d93810a6bdb654400a9412abe70
mpredki99 <mpredki99@gmail.com>
2025-07-13 20:55:00 +0200

    Add observation_equations and robust module

--- src/pysurv/adjustment/__init__.py
+++ src/pysurv/adjustment/__init__.py
@@ -1,4 +1,10 @@
 from .adjustment import Adjustment
 from .sigma_config import sigma_config
+from . import robust, observation_equations
 
-__all__ = ["Adjustment", "sigma_config"]
+__all__ = [
+    "Adjustment", 
+    "observation_equations", 
+    "robust", 
+    "sigma_config"
+]
