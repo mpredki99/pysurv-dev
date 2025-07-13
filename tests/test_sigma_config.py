ad24313e2e22bd8195040350acc4ddefee76169c
mpredki99 <mpredki99@gmail.com>
2025-07-13 14:20:49 +0200

    remove adjustment object from test

--- tests/test_sigma_config.py
+++ tests/test_sigma_config.py
@@ -1,4 +1,4 @@
-from pysurv.adjustment import Adjustment, sigma_config
+from pysurv.adjustment import sigma_config
 
 
 class TestSigmaConfig:
@@ -6,12 +6,4 @@ class TestSigmaConfig:
         sigma_config_1 = sigma_config
         sigma_config_2 = sigma_config
 
-        adjustment_1 = Adjustment()
-        adjustment_2 = Adjustment()
-
-        assert (
-            sigma_config_1
-            is sigma_config_2
-            is adjustment_1.sigma_config
-            is adjustment_2.sigma_config
-        )
+        assert sigma_config_1 is sigma_config_2
